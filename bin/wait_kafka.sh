1a03d8efd0a816f8cb07508539a920c8e644047d
Lorenzo Nicora <lorenzo.nicora@nutmeg.com>
2019-01-09 16:28:38 +0000

    Updating docker-compose to be more OSX Docker friendly

--- bin/wait_kafka.sh
+++ bin/wait_kafka.sh
@@ -2,6 +2,6 @@
 
 echo "Wait for all brokers to be up-and-running"
 docker-compose logs zk1 | grep -iq "binding" && echo "zk1 up"
-docker-compose logs -f kafka-1 | grep -q "started (kafka.server.KafkaServer)" && echo "kafka-1 up"
-docker-compose logs -f kafka-2 | grep -q "started (kafka.server.KafkaServer)" && echo "kafka-2 up"
-docker-compose logs -f kafka-3 | grep -q "started (kafka.server.KafkaServer)" && echo "kafka-3 up"
+docker-compose logs -f kafka1 | grep -q "started (kafka.server.KafkaServer)" && echo "kafka1 up"
+docker-compose logs -f kafka2 | grep -q "started (kafka.server.KafkaServer)" && echo "kafka2 up"
+docker-compose logs -f kafka3 | grep -q "started (kafka.server.KafkaServer)" && echo "kafka3 up"
